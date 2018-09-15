bd83b2fef029cdddbc9af2f0a14ac19893fc1a42
雷日锦 <362606856@qq.com>
2018-09-15 15:39:54 +0800

    update

--- bin/diy.php
+++ bin/diy.php
@@ -1,15 +1,19 @@
 <?php
-$files=array(
-	"../html/forum/forum.css",	 
+$shopCss['name']="shop.css";
+$shopCss['files']=array(
+	"../html/forum/forum.css",
+	"../html/category/index.css",
+	"../html/cart/index.css"
 );
+$files= $shopCss;
 $html="";
-foreach($files as $file){
+foreach($files['files'] as $file){
 	$html.=file_get_contents($file);
 }
-file_put_contents("../dist/h5/forum.css",$html);
+file_put_contents("../dist/h5/".$files['name'],$html);
 //2.2px
 $html=preg_replace_callback("/(\d+)px/",function($res){
 	return ($res[1]*2.2)."px";
 },$html);
-file_put_contents("../dist/uni/forum.css",$html);
+file_put_contents("../dist/uni/".$files['name'],$html);
 ?>
